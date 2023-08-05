264a5e761ccde48990c89beba2026758a8b25bcd
Yevhen Bodnia <bodnya29179@gmail.com>
2023-08-05 20:41:19 +0300

    Add base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ProductComponent, ProductsComponent, CartComponent } from './components';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    ProductsComponent,
+    CartComponent,
+  ],
   imports: [BrowserModule],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
